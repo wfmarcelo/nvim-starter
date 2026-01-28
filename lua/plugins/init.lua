606afac890d7b76aeaa56ebff84d7f86239a7221
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2026-01-28 15:58:37 -0300

    ajustado configurações de formatação

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,42 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format { async = true, lsp_format = "fallback" }
+        end,
+        mode = "",
+        desc = "[F]ormat buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        cs = { "lsp" },
+        sql = { "sql_formatter" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+      formatters = {
+        stylua = {
+          -- The "-" at the end tells stylua to use STDIN (standard input)
+          -- This allows it to format code that isn't saved to a file yet.
+          args = { "--indent-type", "Spaces", "--indent-width", "2", "-" },
+          -- This allows the formatter to run even if no config file is found
+          stdin = true,
+        },
+      },
+    },
   },
-
+  {
+    "NMAC427/guess-indent.nvim",
+  },
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",

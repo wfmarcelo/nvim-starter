b1e95bfef0bae0c273b98e992f65a090c5824887
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2026-03-08 08:46:26 -0300

    feat(core): enable autoread and checktime autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -2,6 +2,16 @@ require "nvchad.autocmds"
 
 local autocmd = vim.api.nvim_create_autocmd
 
+-- check for file changes when buffer gains focus or enters
+autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  callback = function()
+    if vim.fn.getcmdwintype() == "" then
+      vim.cmd "checktime"
+    end
+  end,
+})
+
 autocmd("FileType", {
   pattern = { "sql", "mysql", "plsql" },
   callback = function()
--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,7 @@
 require "nvchad.options"
 
 -- add yours here!
+vim.opt.autoread = true
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!

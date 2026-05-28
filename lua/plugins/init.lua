48ce8b801b4a1c9257e13b039bb00ac1d35d31aa
Marcelo Wollmann Figueiró <marcelo.wollmann@gmail.com>
2026-05-28 10:32:33 -0300

    avante: configuração do provider padrão

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -159,7 +159,7 @@ return {
 
     opts = {
       instructions_file = "avante.md",
-      provider = "ollama",
+      provider = "gemini",
       providers = {
         ollama = {
           model = "qwen2.5-coder:7b",
@@ -172,6 +172,14 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     },
   },
   {
